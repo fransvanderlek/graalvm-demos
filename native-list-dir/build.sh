fc955d4c82699e89c0473896dba54e0090330371
fransvanderlek <fransvanderlek@gmail.com>
2021-04-11 22:09:00 +0200

    Update build.sh

--- native-list-dir/build.sh
+++ native-list-dir/build.sh
@@ -6,4 +6,4 @@ $GRAALVM_HOME/bin/native-image ListDir
 
 #javac ExtListDir.java
 #$GRAALVM_HOME/bin/native-image --js ExtListDir
-docker build --env GRAALVM_HOME=$GRAALVM_HOME . -t listdir:latest
+docker build --build-arg GRAALVM_HOME=$GRAALVM_HOME . -t listdir:latest
